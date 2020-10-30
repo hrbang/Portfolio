eafc47441e68070a5deebbd78083921678d7ec51
Jonas Bang <jonasbang@hotmail.dk>
2020-10-31 00:21:15 +0100

    added GSAP

--- pages/index.js
+++ pages/index.js
@@ -2,57 +2,74 @@ import Head from "next/head";
 import Link from 'next/link';
 import Cursor from '../components/cursor'
 
-export default function Home() {
-  return (
-    <div>
-    	<Head>
-        	<title>Create Next App</title>
-        	<link rel="icon" href="/favicon.ico" />
-    	</Head>
+import SplitText from 'gsap/all'
 
-		<Cursor />
-    	<main className="jb_frontpage">
-			<div className="jb_front__wrapper">
-				<div className="jb_front__topnav">
-					<div className="jb_front__topnav_logo">
-						<p className="logo">
-							"Portfolio"
+
+export default function Home() {
+
+	const childSplit = new SplitText("h1", {
+		type: "lines",
+		linesClass: "split-child"
+	});
+
+	const parentSplit = new SplitText("h1", {
+		linesClass: "split-parent"
+	});
+
+	gsap.from(childSplit.lines, {
+		duration: 1.5,
+		yPercent: 100,
+		ease: "power4",
+		stagger: 0.1
+	});
+
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Cursor />
+			<main className="jb_frontpage">
+				<div className="jb_front__wrapper">
+					<div className="jb_front__topnav">
+						<div className="jb_front__topnav_logo">
+							<p className="logo">
+								"Portfolio"
 						</p>
+						</div>
+						<div className="jb_front__topnav_about">
+							<Link href="#">
+								<a>About</a>
+							</Link>
+						</div>
 					</div>
-					<div className="jb_front__topnav_about">
-						<Link href="#">
-							<a>About</a>
-						</Link>
-					</div>
-				</div>
-				<div className="jb_front__title">
-					<h1 className="title">
-						<span>Hi!</span>
-						<span>I am Jonas Bang</span>
-						<span>an independent creative designer</span>
-						<span>based in Denmark.</span>
+					<div className="jb_front__title">
+						<h1 className="title">
+							Hi! <br />I am Jonas Bang <br />an independent creative designer <br />based in Denmark.
 					</h1>
-				</div>
-				<div className="jb_front__social">
-					<ul>
-						<li>
-							<Link href="REMEMBER ME">
-								<a>Behance</a>
-							</Link>
-						</li>
-						<li>
-							<Link href="REMEMBER ME">
-								<a>Instagram</a>
-							</Link>
-						</li>
-						<li>
-							<Link href="REMEMBER ME">
-								<a>Mail</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-				{/* <div className="jb_front__botnav_projects">
+					</div>
+					<div className="jb_front__social">
+						<ul>
+							<li>
+								<Link href="REMEMBER ME">
+									<a>Behance</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="REMEMBER ME">
+									<a>Instagram</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="REMEMBER ME">
+									<a>Mail</a>
+								</Link>
+							</li>
+						</ul>
+					</div>
+					{/* <div className="jb_front__botnav_projects">
 					<Link href="REMEMBER ME">
 						<a>Projects</a>
 					</Link>
@@ -62,8 +79,8 @@ export default function Home() {
 						<a>Show Reel</a>
 					</Link>
 				</div> */}
-			</div>
-		</main>
-    </div>
-  );
+				</div>
+			</main>
+		</div>
+	);
 }
